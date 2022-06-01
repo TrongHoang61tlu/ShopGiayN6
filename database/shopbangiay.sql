6a2204ce5e9b8f54e77b26c46014867cbae49f65
bimbimsnackk <anhlaso80@gmail.com>
2022-06-01 22:14:07 +0700

    update sql

--- database/shopbangiay.sql
+++ database/shopbangiay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 18, 2022 lúc 02:22 PM
+-- Thời gian đã tạo: Th6 01, 2022 lúc 08:20 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.10
 
@@ -24,40 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chitiethd`
---
-
-CREATE TABLE `chitiethd` (
-  `SoHD` int(11) NOT NULL,
-  `MaHang` int(11) NOT NULL,
-  `Slg` int(11) DEFAULT NULL,
-  `TyLeGiamGia` float DEFAULT 0,
-  `ThanhTien` float DEFAULT 0 CHECK (`TyLeGiamGia` >= 0 and `TyLeGiamGia` < 1)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `chitiethdnhap`
---
-
-CREATE TABLE `chitiethdnhap` (
-  `SoHDNhap` int(11) NOT NULL,
-  `MaHang` int(11) NOT NULL,
-  `SlgNhap` int(11) DEFAULT NULL,
-  `GiaNhap` float DEFAULT NULL,
-  `TienNhap` float DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `hang`
 --
 
 CREATE TABLE `hang` (
   `MaHang` int(11) NOT NULL,
   `TenHang` varchar(255) DEFAULT NULL,
+  `anhSP` varchar(255) NOT NULL,
   `MauSac` varchar(10) DEFAULT NULL,
   `Size` char(15) DEFAULT NULL,
   `DVT` varchar(30) DEFAULT NULL,
@@ -66,18 +39,12 @@ CREATE TABLE `hang` (
   `NgungBan` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Cấu trúc bảng cho bảng `hdnhap`
+-- Đang đổ dữ liệu cho bảng `hang`
 --
 
-CREATE TABLE `hdnhap` (
-  `SoHDNhap` int(11) NOT NULL,
-  `NgayNhap` date DEFAULT NULL,
-  `TongTienNhap` float DEFAULT 0,
-  `MaNCC` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `hang` (`MaHang`, `TenHang`, `anhSP`, `MauSac`, `Size`, `DVT`, `SlgTon`, `GiaBan`, `NgungBan`) VALUES
+(1, 'Faded', '', 'do', '1', 'test', 10, 100000, b'1');
 
 -- --------------------------------------------------------
 
@@ -90,6 +57,7 @@ CREATE TABLE `hoadon` (
   `NgayBan` date DEFAULT NULL,
   `MaNV` int(10) NOT NULL,
   `MaKh` int(10) NOT NULL,
+  `MaHang` int(11) NOT NULL,
   `TongTien` float DEFAULT 0,
   `HoanThanh` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -120,7 +88,8 @@ INSERT INTO `khach` (`MaKh`, `TenKh`, `Emailkh`, `Passwordkh`, `GioiTinhKHACH`, 
 (1, 'Hoàng', 'hoangne@gmail.com', '202cb962ac59075b964b07152d234b70', NULL, NULL, NULL, 0, ''),
 (2, 'Hoàng', 'admin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', NULL, NULL, NULL, 0, ''),
 (3, '', '', 'd41d8cd98f00b204e9800998ecf8427e', NULL, NULL, NULL, 0, ''),
-(8, 'Hoàng', 'tronghoang19112001@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', NULL, NULL, NULL, 1, '7aa2ed232110787e8e9b49a219494e78');
+(8, 'Hoàng', 'tronghoang19112001@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', NULL, NULL, NULL, 1, '7aa2ed232110787e8e9b49a219494e78'),
+(15, 'Hoàng', 'nguyenyenngoc61ht@gmail.com', '698d51a19d8a121ce581499d7b701668', NULL, NULL, NULL, 0, '01a8168f55130b3da1b68c16044c10bf');
 
 -- --------------------------------------------------------
 
@@ -134,19 +103,12 @@ CREATE TABLE `luongnv` (
   `Luong` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Cấu trúc bảng cho bảng `nhacungcap`
+-- Đang đổ dữ liệu cho bảng `luongnv`
 --
 
-CREATE TABLE `nhacungcap` (
-  `MaNCC` int(11) NOT NULL,
-  `TenNCC` varchar(255) DEFAULT NULL,
-  `SđtNCC` char(12) DEFAULT NULL,
-  `DiaChiNCC` varchar(255) DEFAULT NULL,
-  `ConGiaoDich` bit(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `luongnv` (`MaCV`, `ChucVu`, `Luong`) VALUES
+(1, 'sep', 10);
 
 -- --------------------------------------------------------
 
@@ -162,6 +124,7 @@ CREATE TABLE `nhanvien` (
   `SđtNV` char(10) DEFAULT NULL,
   `EmailNV` varchar(255) NOT NULL,
   `PasswordNV` varchar(255) NOT NULL,
+  `permission` int(5) NOT NULL,
   `DiaChiNV` varchar(30) DEFAULT NULL,
   `DaNghi` bit(1) DEFAULT NULL,
   `MaCV` int(11) DEFAULT NULL
@@ -172,31 +135,10 @@ CREATE TABLE `nhanvien` (
 --
 
 --
--- Chỉ mục cho bảng `chitiethd`
---
-ALTER TABLE `chitiethd`
-  ADD PRIMARY KEY (`SoHD`,`MaHang`),
-  ADD KEY `MaHang` (`MaHang`);
-
---
--- Chỉ mục cho bảng `chitiethdnhap`
---
-ALTER TABLE `chitiethdnhap`
-  ADD PRIMARY KEY (`MaHang`,`SoHDNhap`),
-  ADD KEY `SoHDNhap` (`SoHDNhap`);
-
---
 -- Chỉ mục cho bảng `hang`
 --
 ALTER TABLE `hang`
   ADD PRIMARY KEY (`MaHang`);
-
---
--- Chỉ mục cho bảng `hdnhap`
---
-ALTER TABLE `hdnhap`
-  ADD PRIMARY KEY (`SoHDNhap`),
-  ADD KEY `MaNCC` (`MaNCC`);
 
 --
 -- Chỉ mục cho bảng `hoadon`
@@ -204,7 +146,8 @@ ALTER TABLE `hdnhap`
 ALTER TABLE `hoadon`
   ADD PRIMARY KEY (`SoHD`),
   ADD KEY `MaNV` (`MaNV`),
-  ADD KEY `MaKh` (`MaKh`);
+  ADD KEY `MaKh` (`MaKh`),
+  ADD KEY `MaHang` (`MaHang`);
 
 --
 -- Chỉ mục cho bảng `khach`
@@ -217,12 +160,6 @@ ALTER TABLE `khach`
 --
 ALTER TABLE `luongnv`
   ADD PRIMARY KEY (`MaCV`);
-
---
--- Chỉ mục cho bảng `nhacungcap`
---
-ALTER TABLE `nhacungcap`
-  ADD PRIMARY KEY (`MaNCC`);
 
 --
 -- Chỉ mục cho bảng `nhanvien`
@@ -239,13 +176,7 @@ ALTER TABLE `nhanvien`
 -- AUTO_INCREMENT cho bảng `hang`
 --
 ALTER TABLE `hang`
-  MODIFY `MaHang` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `hdnhap`
---
-ALTER TABLE `hdnhap`
-  MODIFY `SoHDNhap` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaHang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `hoadon`
@@ -257,56 +188,31 @@ ALTER TABLE `hoadon`
 -- AUTO_INCREMENT cho bảng `khach`
 --
 ALTER TABLE `khach`
-  MODIFY `MaKh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `MaKh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `luongnv`
 --
 ALTER TABLE `luongnv`
-  MODIFY `MaCV` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `nhacungcap`
---
-ALTER TABLE `nhacungcap`
-  MODIFY `MaNCC` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaCV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `nhanvien`
 --
 ALTER TABLE `nhanvien`
-  MODIFY `MaNV` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaNV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Các ràng buộc cho bảng `chitiethd`
---
-ALTER TABLE `chitiethd`
-  ADD CONSTRAINT `chitiethd_ibfk_1` FOREIGN KEY (`SoHD`) REFERENCES `hoadon` (`SoHD`),
-  ADD CONSTRAINT `chitiethd_ibfk_2` FOREIGN KEY (`MaHang`) REFERENCES `hang` (`MaHang`);
-
---
--- Các ràng buộc cho bảng `chitiethdnhap`
---
-ALTER TABLE `chitiethdnhap`
-  ADD CONSTRAINT `chitiethdnhap_ibfk_1` FOREIGN KEY (`SoHDNhap`) REFERENCES `hdnhap` (`SoHDNhap`),
-  ADD CONSTRAINT `chitiethdnhap_ibfk_2` FOREIGN KEY (`MaHang`) REFERENCES `hang` (`MaHang`);
-
---
--- Các ràng buộc cho bảng `hdnhap`
---
-ALTER TABLE `hdnhap`
-  ADD CONSTRAINT `hdnhap_ibfk_1` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`);
-
---
 -- Các ràng buộc cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
   ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`),
-  ADD CONSTRAINT `hoadon_ibfk_2` FOREIGN KEY (`MaKh`) REFERENCES `khach` (`MaKh`);
+  ADD CONSTRAINT `hoadon_ibfk_2` FOREIGN KEY (`MaKh`) REFERENCES `khach` (`MaKh`),
+  ADD CONSTRAINT `hoadon_ibfk_3` FOREIGN KEY (`MaHang`) REFERENCES `hang` (`MaHang`);
 
 --
 -- Các ràng buộc cho bảng `nhanvien`
